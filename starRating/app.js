56dcb1bd0651f2e4bd3febff8eb5e4448432eea7
AlexanderVichev <116145973+AlexanderVichev@users.noreply.github.com>
2024-07-12 04:30:44 +0300

    Add files via upload

---
+++ starRating/app.js
@@ -0,0 +1,9 @@
+const stars = document.querySelectorAll(".stars i");
+
+stars.forEach((star, i) => {
+  star.addEventListener("click", () => {
+    stars.forEach((star, j) => {
+      i >= j ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
